c72a39c3754f11a4784bf2751fe1997f492659f6
GD20170203 <40146720170203@litec.ac.at>
2022-08-10 12:29:20 +0200

    init refactor

---
+++ src/src/contexts/AuthContext.tsx
@@ -0,0 +1,67 @@
+import React, { useContext, useState, useEffect, ReactNode } from "react";
+import firebase from "firebase/compat";
+import { auth } from "../proxy/firebase";
+
+type AuthProviderProps = {
+  children: ReactNode
+}
+
+const AuthContext = React.createContext<firebase.User | null>(null);;
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [currentUser, setCurrentUser] = useState<firebase.User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  function signup(email: string, password: string) {
+    return auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  function login(email: string, password: string) {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+
+  function logout() {
+    return auth.signOut();
+  }
+
+  function resetPassword(email: string) {
+    return auth.sendPasswordResetEmail(email);
+  }
+
+  function updateEmail(email: string) {
+    return currentUser?.updateEmail(email);
+  }
+
+  function updatePassword(password: string) {
+    return currentUser?.updatePassword(password);
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    login,
+    signup,
+    logout,
+    resetPassword,
+    updateEmail,
+    updatePassword,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
